c6ab5ed436f8b6d5c4cbaf7c7ec1ba77cf599e03
Phoshoko Thabang <phoshokothabang1@gmail.com>
2022-08-24 03:58:23 +0000

    python

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,4 +1,4 @@
-fndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 /**
@@ -11,8 +11,8 @@ fndef LISTS_H
  */
 typedef struct listint_s
 {
-	    int n;
-	        struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
