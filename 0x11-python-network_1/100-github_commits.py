a7ed1df45b04389d2d32b989a463d7ccb5ef1731
Phoshoko Thabang <phoshokothabang1@gmail.com>
2022-12-16 19:53:24 +0000

    Python - Network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
         for i in range(10):
             print(student[i].get('sha'), student[i].get('commit')
                   .get('author').get('name'), sep=": ")
-    except:
+    except student.doestnotexist:
         pass
