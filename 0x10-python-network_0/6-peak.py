7254f4c64054d0d416745e264485e503618d66a9
Phoshoko Thabang <phoshokothabang1@gmail.com>
2022-12-21 21:28:54 +0000

    Network Update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,40 +2,40 @@
 """Write a function that finds a peak in a list of unsorted integers."""
 
 
-def peak_rec(arr, l, r):
+def peak_rec(arr, lst, r):
     """Recursive way"""
-    if l == r:
-        return arr[l]
-    if r - l == 1:
-        return max(arr[r], arr[l])
+    if lst == r:
+        return arr[lst]
+    if r - lst == 1:
+        return max(arr[r], arr[lst])
     m = (r + l) // 2
     if arr[m] < arr[m + 1]:
         return peak_rec(arr, m + 1, r)
-    return peak_rec(arr, l, m)
+    return peak_rec(arr, lst, m)
 
 
 def find_peak_rec(list_of_integers):
     """Function to find a peak in a list"""
     if list_of_integers == []:
         return None
-    l = len(list_of_integers)
+    lst = len(list_of_integers)
     arr = list_of_integers
-    return peak_rec(arr, 0, l - 1)
+    return peak_rec(arr, 0, lst - 1)
 
 
 def find_peak_0(list_of_integers):
     """Function to find a peak in a list"""
     if list_of_integers == []:
         return None
-    l = len(list_of_integers)
-    m = l // 2
+    lst = len(list_of_integers)
+    m = lst // 2
     arr = list_of_integers
     while True:
-        if m != 0 and m != l - 1 and arr[m - 1] < arr[m] < arr[m + 1]:
-            m = (l + m) // 2
-        elif m != 0 and m != l - 1 and arr[m - 1] > arr[m] > arr[m + 1]:
+        if m != 0 and m != lst - 1 and arr[m - 1] < arr[m] < arr[m + 1]:
+            m = (lst + m) // 2
+        elif m != 0 and m != lst - 1 and arr[m - 1] > arr[m] > arr[m + 1]:
             m = m // 2
-        elif m != 0 and m != l - 1 and arr[m - 1] > arr[m] < arr[m + 1]:
+        elif m != 0 and m != lst - 1 and arr[m - 1] > arr[m] < arr[m + 1]:
             m = m // 2
         else:
             return arr[m]
@@ -45,14 +45,14 @@ def find_peak(list_of_integers):
     """Function to find a peak in a list"""
     if list_of_integers == []:
         return None
-    l = len(list_of_integers)
-    m = l // 2
+    lst = len(list_of_integers)
+    m = lst // 2
     arr = list_of_integers
     while True:
-        if m in (0, l - 1) or arr[m - 1] < arr[m] > arr[m + 1]:
+        if m in (0, lst - 1) or arr[m - 1] < arr[m] > arr[m + 1]:
             return arr[m]
         if arr[m] < arr[m + 1]:
-            m = (l + m) // 2
+            m = (lst + m) // 2
         else:
             m = m // 2
 
